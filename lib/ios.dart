da20c572f8c12049026d5691f2797434e3e73cd1
Lucas <6011385+lcsvcn@users.noreply.github.com>
2023-10-12 03:40:41 -0300

    Update ios.dart

--- lib/ios.dart
+++ lib/ios.dart
@@ -52,7 +52,7 @@ final List<_IosLaunchImageTemplate> _iOSBrandingImagesDark =
 
 /// Create iOS splash screen
 void _createiOSSplash({
-  bool shouldSkipImage = false,
+  required bool shouldSkipImage,
   required String? imagePath,
   required String? darkImagePath,
   String? brandingImagePath,
